448969f94830444d6d04e783bc99b6cc4b36e927
xing <xingxingli2017@gmail.com>
2018-08-21 23:10:33 -0400

    main init

--- src/components/App.js
+++ src/components/App.js
@@ -2,15 +2,14 @@ import React, { Component } from 'react';
 import logo from '../assets/logo.svg';
 import '../styles/App.css';
 import {TopNavBar} from "./TopNavBar"
+import {Main} from "./Main"
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <TopNavBar />
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Main />
       </div>
     );
   }
